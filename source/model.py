9360a911855f9467914774c220b7eb29ee69fe48
Radeen Mirzaie <radinms@gmail.com>
2022-04-26 16:29:18 +0200

    Updated script with cross validation

--- source/model.py
+++ source/model.py
@@ -1,36 +1,108 @@
-import json
+import ast
 from sys import argv
 import pandas as pd
+import numpy as np
 from sklearn.decomposition import PCA
-import json
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score, GridSearchCV, KFold
+from sklearn.metrics import accuracy_score
+from numpy import mean
+from numpy import std
+
+
+
+def classify(file, dict1):
+    all_keys = [key[1] for key in dict1.keys()]
+    #file = file.drop(file.loc[file['Start'] != all_keys[0]], axis=0)
+    #file = file.loc[file['Start'] == all_keys[0]]
+
+    # Drop everything but the selected regions
+    for idx, row in file.iterrows():
+        if row['Start'] not in all_keys:
+            file.drop(idx, inplace=True)
+
+    #file.to_csv('Train_117.csv')
+
+    file = file.transpose()
+
+    # Drop the first 4 columns eg. Chromosome, Start, etc.
+    file.drop(['Chromosome', 'Start', 'End', 'Nclone'], inplace=True)
+
+    with open('Train_clinical', 'r') as f:
+        f = pd.read_csv(f, delimiter='\t')
+
+    labels = np.array(f['Subgroup'])
+    features = np.array(file)
+    features_list = list(file.columns)
+
+    X = features
+    y = labels
+
+    # configure the cross-validation procedure
+    cv_outer = KFold(n_splits=5, shuffle=True, random_state=42)
+    # enumerate splits
+    outer_results = list()
+    for train_ix, test_ix in cv_outer.split(X):
+        # split data
+        X_train, X_test = X[train_ix, :], X[test_ix, :]
+        y_train, y_test = y[train_ix], y[test_ix]
+        # configure the cross-validation procedure
+        cv_inner = KFold(n_splits=4, shuffle=True, random_state=42)
+        # define the model
+        model = RandomForestClassifier(random_state=42)
+        # define search space
+        space = dict()
+        space['n_estimators'] = [10, 100, 500]
+        space['max_features'] = [10, 20, 50]
+        # define search
+        search = GridSearchCV(model, space, scoring='accuracy', cv=cv_inner, refit=True)
+        # execute search
+        result = search.fit(X_train, y_train)
+        # get the best performing model fit on the whole training set
+        best_model = result.best_estimator_
+        # evaluate model on the hold out dataset
+        yhat = best_model.predict(X_test)
+        # evaluate the model
+        acc = accuracy_score(y_test, yhat)
+        # store the result
+        outer_results.append(acc)
+        # report progress
+        print('>acc=%.3f, est=%.3f, cfg=%s' % (acc, result.best_score_, result.best_params_))
+    # summarize the estimated performance of the model
+    print('Accuracy: %.3f (%.3f)' % (mean(outer_results), std(outer_results)))
+
+
 
 
 def pre_process(file, file2):
-    #file.drop(['Chromosome', 'Start', 'End', 'Nclone'], axis=1, inplace=True)
-    #print(file.head())
 
-    #print(file['Start'])
-
-    flag = True
-
+    flag = False
     if flag:
         # store all region of the genome into a dict
         dict1 = {}
-        for start, end in zip(file.Start, file.End):
-            dict1[start,end] = []
+        for chrm, start, end in zip(file.Chromosome,file.Start, file.End):
+            dict1[chrm,start,end] = []
+
 
         # map all genes to the genome
         for idx, row in file2.iterrows():
             for key in dict1.keys():
-                if row['Gene_start'] >= key[0] and row['Gene_end'] <= key[1]:
+                if int(row['Chromosome']) == key[0] and row['Gene_start'] >= key[1] and row['Gene_end'] <= key[2]:
                     dict1[key].append(row['ENSEMBL_gene_id'])
 
+        with open('gene_map.txt', 'w') as output:
+            output.write(str(dict1))
 
-        #print(dict1)
 
-        #with open('region_to_gene.txt', 'w') as output:
-        #    output.write(json.dumps(dict1))
+    flag = True
+    if flag:
 
+        with open('gene_map.txt', 'r') as f2:
+            f2 = f2.read()
+
+        dict1 = ast.literal_eval(f2)
 
         with open('gene_list.txt', 'r') as f:
             gene_list = f.readlines()
@@ -51,14 +123,20 @@ def pre_process(file, file2):
 
                 else:
                     not_in_value += 1
-        print(dict2)
-        print(not_in_value)
 
+        print('Length of dict1 keys %i:' % len(dict1.keys()))
+        print('Length of dic2 keys %i:' % len(dict2.keys()))
 
+    flag = False
+    if flag:
+        f = open("final_regions.txt", "w")
+        f.write("{\n")
+        for k in dict2.keys():
+            f.write("'{}':'{}'\n".format(k, dict2[k]))
+        f.write("}")
+        f.close()
 
-
-
-
+    return classify(file, dict2)
 
 
 
@@ -79,7 +157,10 @@ def main():
 
     file = pd.read_csv(f_in, delimiter='\t')
     file2 = pd.read_csv(f_in2, delimiter='\t')
-    print(file2.head())
+
+    for idx, row in file2.iterrows():
+        if row['Chromosome'] == 'X' or row['Chromosome'] == 'Y':
+            file2.drop(idx, axis=0, inplace=True)
 
     # call functions
     pre_process(file, file2)
@@ -87,11 +168,8 @@ def main():
 
 
 
-
 if __name__ == '__main__':
     main()
-
-
 
 
 
